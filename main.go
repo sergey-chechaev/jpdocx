0ce50ab5bb16c4125556dcceac7ed9e1cecf88f9
Sergey Chechaev <2netme@gmail.com>
2017-02-05 00:55:48 +0300

    fix tabs and spaces

--- main.go
+++ main.go
@@ -1,11 +1,11 @@
 package main
 
 import (
-    "jpdocx/docx"
-    "fmt"
-    "encoding/json"
-    "os"
-		"io/ioutil"
+  "jpdocx/docx"
+  "fmt"
+  "encoding/json"
+  "os"
+  "io/ioutil"
 )
 
 type Language struct {
@@ -15,30 +15,30 @@ type Language struct {
 
 func main() {
   bytes, err_stdin := ioutil.ReadAll(os.Stdin)
-	if err_stdin != nil {
-			panic(err_stdin)
-	}
+  if err_stdin != nil {
+    panic(err_stdin)
+  }
   path := os.Args[1]
   path_to := os.Args[2]
-	path_str := fmt.Sprintf("%v", path)
-	path_to_str := fmt.Sprintf("%v", path_to)
+  path_str := fmt.Sprintf("%v", path)
+  path_to_str := fmt.Sprintf("%v", path_to)
 
-	r, err := docx.ReadDocxFile(path_str)
-	if err != nil {
-			panic(err)
-	}
-
-	var languages []Language
-	json.Unmarshal(bytes, &languages)
-
-	docx1 := r.Editable()
-	for l := range languages {
-		str_key := fmt.Sprintf("{{%v}}", languages[l].Key)
-		docx1.Replace(str_key, languages[l].Value, -1)
+  r, err := docx.ReadDocxFile(path_str)
+  if err != nil {
+      panic(err)
   }
 
-	docx1.WriteToFile(path_to_str)
+  var languages []Language
+  json.Unmarshal(bytes, &languages)
 
-	r.Close()
-	fmt.Println("ok")
+  docx1 := r.Editable()
+  for l := range languages {
+    str_key := fmt.Sprintf("{{%v}}", languages[l].Key)
+    docx1.Replace(str_key, languages[l].Value, -1)
+  }
+
+  docx1.WriteToFile(path_to_str)
+
+  r.Close()
+  fmt.Println("ok")
 }
