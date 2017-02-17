ecba03ba202d9d2fc585b4b2e32f41db00995bac
Sergey Chechaev <2netme@gmail.com>
2017-02-17 15:09:33 +0300

    refactoring hash to json key val

--- jpdocx.go
+++ jpdocx.go
@@ -2,37 +2,36 @@ package main
 
 import (
 	"encoding/json"
-	"fmt"
+  "fmt"
 	"jpdocx/docx"
-	"os"
+  "os"
 )
 
-type Hash struct {
-	Key   string
-	Value string
-}
-
-func main() {
+func main(){
   params := os.Args[1]
-  bytes := []byte(params)
 	path := os.Args[2]
 	path_to := os.Args[3]
 	path_str := fmt.Sprintf("%v", path)
 	path_to_str := fmt.Sprintf("%v", path_to)
+  data := []byte(params)
+
+  objmap := make(map[string]interface{})
+  err := json.Unmarshal(data, &objmap)
+  if err != nil {
+    fmt.Println(err)
+  }
 
 	r, err := docx.ReadDocxFile(path_str)
 	if err != nil {
 		panic(err)
 	}
-	var hash_keys []Hash
-	json.Unmarshal(bytes, &hash_keys)
 
 	docx1 := r.Editable()
-	for l := range hash_keys {
-		str_key := fmt.Sprintf("{{%v}}", hash_keys[l].Key)
-		docx1.Replace(str_key, hash_keys[l].Value, -1)
-	}
-
+  for key, value := range objmap {
+		str_key := fmt.Sprintf("{{%v}}", key)
+		str_value := fmt.Sprintf("%v", value)
+		docx1.Replace(str_key, str_value, -1)
+  }
 	docx1.WriteToFile(path_to_str)
 
 	r.Close()
